7003fa740615666c6bc8100d31d94c8b6516e16c
Kimberly Lee <kimberly.lee@salesforce.com>
2017-12-13 16:58:37 -0800

    Added more documentation and tests

--- test/lib/elasticache_test.exs
+++ test/lib/elasticache_test.exs
@@ -18,6 +18,27 @@ defmodule ExAws.ElastiCacheTest do
     }
   end
 
+  test "authorize_cache_security_group_ingress" do
+    expected = build_request(:authorize_cache_security_group_ingress,
+      %{
+        "CacheSecurityGroupName" => "MyGroup",
+        "EC2SecurityGroupName" => "default",
+        "EC2SecurityGroupOwnerId" => "1234-5678-1234"
+        })
+
+    assert expected == ElastiCache.authorize_cache_security_group_ingress("MyGroup", "default", "1234-5678-1234")
+  end
+
+  test "copy_snapshot" do
+    expected = build_request(:copy_snapshot,
+      %{
+        "SourceSnapshotName" => "SourceSnap",
+        "TargetSnapshotName" => "TargetSnap"
+        })
+
+    assert expected == ElastiCache.copy_snapshot("SourceSnap", "TargetSnap")
+  end
+
   test "create_cache_cluster no optional params" do
     expected = build_request(:create_cache_cluster,
       %{
@@ -163,6 +184,30 @@ defmodule ExAws.ElastiCacheTest do
       ])
   end
 
+  test "describe_cache_engine_versions no optional params" do
+    assert build_request(:describe_cache_engine_versions) == ElastiCache.describe_cache_engine_versions
+  end
+
+  test "describe_cache_engine_versions with optional params" do
+    expected = build_request(:describe_cache_engine_versions,
+      %{
+        "CacheParameterGroupFamily" => "memcached1.4",
+        "DefaultOnly" => false,
+        "Engine" => "memcached",
+        "EngineVersion" => "1.4.14",
+        "MaxRecords" => 200
+        })
+
+    assert expected == ElastiCache.describe_cache_engine_versions(
+      [
+        cache_parameter_group_family: "memcached1.4",
+        default_only: false,
+        engine: "memcached",
+        engine_version: "1.4.14",
+        max_records: 200
+      ])
+  end
+
   test "describe_replication_groups no optional params" do
     assert build_request(:describe_replication_groups) == ElastiCache.describe_replication_groups
   end
@@ -178,5 +223,17 @@ defmodule ExAws.ElastiCacheTest do
     assert expected == ElastiCache.describe_replication_groups(
       [marker: "TestMarker", max_records: 100, replication_group_id: "TestReplicationGroup"]
     )
+  end
+
+  test "reboot_cache_cluster" do
+    expected = build_request(:reboot_cache_cluster,
+      %{
+        "CacheClusterId" => "MyCache",
+        "CacheNodeIdsToReboot.CacheNodeId.1" => "0001",
+        "CacheNodeIdsToReboot.CacheNodeId.2" => "0002",
+        "CacheNodeIdsToReboot.CacheNodeId.3" => "0003"
+      })
+
+    assert expected == ElastiCache.reboot_cache_cluster("MyCache", ["0001", "0002", "0003"])
   end
 end
